fcfa8d6924eeacc7fd628de0b183d611bbc435da
Jason Brown <jasonkingsley.brown@gmail.com>
2016-02-17 11:38:00 -0600

    add'l exercises

--- README.md
+++ README.md
@@ -7,17 +7,13 @@ In this lab, you will review and work on solidifying your intuition with pointer
 
 ## Exercises
 
-1. Write a function `void pointer_fun(int a, int b)`, that creates two integer pointers named ptrA and ptrB that point to ints a and b, respectively. Print to terminal the values of the pointers. Then, print to terminal the value of the ints at the pointer locations.
+1. Write a function `void pointer_fun(int a, int b)`, that creates two integer pointers named ptrA and ptrB that point to ints a and b, respectively. Print to terminal the values of the ints at the pointers. Then, print to terminal the pointer locations. How far away, in bytes, is location of ptrA versus location of ptrB?
 
-2. Write a function `long * find_max_long_ptr(vector<long> & v)` that finds the max of an integral data set, and returns a pointer to that location.
+2. Write a function `short greater_val(short * ptrA, short * ptrB)` that takes two pointers to shorts, and returns the value of whichever short is greater.
 
-3. Write a function `short greater_val(short * ptrA, short * ptrB)` that takes two pointers to shorts, and returns the value of whichever short is greater.
+3. Write a function `void pointer_arithmetic_chars()` that takes an array of characters (`char[]`)  and iterates over the characters via a pointer to print the sentence.
 
-4. Write a function `void pointer_arithmetic(string & s)` that takes a string, for instance "hello", and prints on one line the `char` at string index 0, and a pointer location to that `char`.  Update the pointer to pointer +2. Then, print the `char` at that pointer location and the pointer location.
-
-## Additional Practice
-
-5. Work through creating a vector of all the students in your section.
+4. Work through creating a vector of all the students in your section.
 
  Use the following class definition:
 ```
@@ -33,4 +29,4 @@ class Student
         int grad_year_;
 }
 ```
-Store pointers to the Students in a global variable `vector<Student *>` enrollees. Create and test the following functions: `void add_student()`, `void remove_student()` , and void `change_grad_year(Student *, int g_yr)`.
+Create and test the following functions: `void add_student()` and `void remove_student()`.
--- lab7.cpp
+++ lab7.cpp
@@ -1,8 +1,100 @@
 #include "eecs230.h"
+#include <vector>
 
+//function declarations
+void pointer_fun(int, int);
+void pointer_arithmetic_chars();
+short greater_val(short *, short *);
+
+void pointer_arithmetic_ints();
+void pointer_data(int);
 
 
 int main()
 {
     cout << "main()" << endl;
+
+    pointer_data(3);
+    //pointer_arithmetic_ints();
+
+
+    //pointer_fun(6,3);
+    //greater_val(...)
+    //pointer_arithmetic_chars();
+
 }
+
+/* Creates two integer pointers named ptrA and ptrB that point to ints a and b, respectively.
+ * Prints to terminal the values of the ints at the pointers. Then, prints to terminal the value of the ints at the pointer locations.*/
+void pointer_fun(int a, int b)
+{
+    //your code here
+
+
+    //follow-up question; how far away, in bytes, is location of ptrA versus location of ptrB? How many bytes is an int stored in?
+    //cout << "\n Int size (bytes): "<< sizeof(a) << endl;
+
+}
+
+void pointer_arithmetic_chars()
+{
+    char course[] = "Programming for Engineers";
+    char *pc = course;
+
+    int length = strlen(course);
+
+    for(int i=0; i<length; i++)
+    {
+        //your code here:
+
+            //print to terminal each letter in char[] by dereferencing pc
+
+            //increment pointer
+
+
+    }
+}
+
+short greater_val(short * ptrA, short * ptrB)
+{
+    //your code here
+
+
+    return 0; // <--- change this
+}
+
+/* prints out pointer location in hex, decimal, and binary */
+void pointer_data(int a)
+{
+    int * ptrA = &a;
+    cout << "ptrA value of int: " << *ptrA << endl;
+    cout << "ptrA virtual memory location: "<< endl;
+    cout << "    (hexadecimal): " << ptrA << endl;
+    cout << "    (decimal)    : " << (long) ptrA << endl;
+    cout << "    (binary)     : " <<  bitset<64>((long) ptrA) << '\n' << endl;
+
+    //Do the same for int b -- your code here
+}
+
+/* prints out ints and pointer addresses */
+void pointer_arithmetic_ints()
+{
+    int array_of_ints[3] = {10, 100, 200};
+    int *ptr;
+
+    // let us have array address in pointer.
+    ptr = array_of_ints;
+    int num_elements = sizeof(array_of_ints) / sizeof(int);
+
+    for (int i = 0; i < num_elements; i++) {
+        cout << "Address of var[" << i << "] = ";
+        cout << ptr << endl;
+
+        cout << "Value of var[" << i << "] = ";
+        cout << *ptr << endl;
+
+        // point to the next location
+        ptr++;
+    }
+}
+
